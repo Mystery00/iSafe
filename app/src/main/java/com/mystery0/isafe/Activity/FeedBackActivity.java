ce4e6f6295c36f7457e8946261188ddf7b5044df
Mystery00 <mystery0dyl520@gmail.com>
2016-08-14 22:06:46 +0800

    a little

--- app/src/main/java/com/mystery0/isafe/Activity/FeedBackActivity.java
+++ app/src/main/java/com/mystery0/isafe/Activity/FeedBackActivity.java
@@ -7,7 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.mystery0.isafe.PublicMethod.ExitApplication;
 import com.mystery0.isafe.R;
+
+import cn.bmob.v3.Bmob;
 
 public class FeedBackActivity extends AppCompatActivity
 {
@@ -17,8 +20,19 @@ public class FeedBackActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_back);
+        ExitApplication.getInstance().addActivity(this);
+        Bmob.initialize(this, getString(R.string.application_id));
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener()
